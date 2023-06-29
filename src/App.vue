841b400c2289c5cf29898ae4f8e2cbd83071dc5b
Lilian Prus <97654292+lilprs@users.noreply.github.com>
2023-06-29 11:19:26 +0200

    implement search field

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
+import SearchField from "./components/SearchField.vue";
 </script>
 
 <template>
   <Navbar sorting="price_ascending" />
+  <div class="crt-search">
+    <SearchField value="" />
+  </div>
+  <div class="crt-results"></div>
 </template>
+<style>
+.crt-search {
+  padding: 5rem;
+  background: white;
+}
+
+.crt-search > .crt-search-field {
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/SearchField.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    :value="props.value"
+    @input="(ev) => emit('update:value', (ev.target as HTMLSelectElement).value)"
+    type="text"
+    class="form-control crt-search-field"
+    placeholder="Wpisz szukaną frazę..."
+  />
+</template>
+<style>
+.crt-search-field {
+  font-size: 2rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+</style>
+<script setup lang="ts">
+import "bootstrap";
+
+const props = defineProps<{
+  value: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:value", value: string): void;
+}>();
+</script>
