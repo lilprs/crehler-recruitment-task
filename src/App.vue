444d9dc82b678cc46be495eb027471728446edc7
Lilian Prus <97654292+lilprs@users.noreply.github.com>
2023-06-29 12:28:05 +0200

    debounce api calls

--- src/App.vue
+++ src/App.vue
@@ -11,39 +11,38 @@ const query = ref("");
 
 <template>
   <Navbar v-model:sorting="sorting" />
-  <div class="crt-search">
+  <div class="crt-search-container">
     <div class="container-fluid">
       <SearchField v-model:value="query" />
     </div>
   </div>
-  <div class="crt-results">
+  <div class="crt-results-container">
     <div class="container-fluid">
       <Results :query="query" :sorting="sorting" />
     </div>
   </div>
 </template>
 <style>
-.crt-search {
+.crt-search-container {
   background: white;
   padding: 4rem 0;
 }
 
-.crt-search .crt-search-field {
+.crt-search-container .crt-search-field {
   max-width: 500px;
   margin: 0 auto;
 }
 
-.crt-results {
+.crt-results-container {
   background: #eee;
   padding: 4rem 0;
+  flex-grow: 1;
 }
 
-.crt-results > div {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 1rem;
-  flex-grow: 1;
-  align-items: flex-start;
+.crt-results-container > div {
   max-width: 960px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 }
 </style>
